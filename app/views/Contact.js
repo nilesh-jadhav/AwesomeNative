c299979ea756e839f125176b17a251ab27da7aae
nilesh-jadhav <30795435+nilesh-jadhav@users.noreply.github.com>
2020-04-21 17:10:05 +0530

    Added navigation and initial route

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -1,16 +1,60 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableHighlight, Alert } from 'react-native';
 import { Header } from '../sections/Header';
 import { Menu } from '../sections/Menu';
-import { Slider } from '../sections/Slider';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export class Contact extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            msg:'Enter Message',
+            name:'Enter Name',
+            email:'Enter your email address'
+        }
+    }
+
+    clearFields = () => { this.setState({ name:'', msg:'', email:'' }) }
+
+    submitDetails = () => {
+        Alert.alert(this.state.name, this.state.msg);
+        this.props.navigation.goBack();
+    }
+
     render() {
         return(
-            <View style={styles.container}>
-                <Header style={styles.section1} message = 'Press to Login' />
-                <Text>Contact us</Text>
-                <Menu/>
+            <View style = { styles.container }>
+                <Header message = 'Press to Login' />
+                <Text style = { styles.heading }>Contact us</Text>
+                <TextInput
+                    style = { styles.inputs }
+                    onChangeText = { (text) => this.setState({ name:text }) }
+                    value = { this.state.name }
+                />
+                <TextInput
+                    style = { styles.multiInput }
+                    onChangeText = { (text) => this.setState({ msg:text }) }
+                    value = { this.state.msg }
+                    multiline = { true }
+                    numberOfLines = { 4 }
+                />
+                <TextInput
+                    style = { styles.inputs }
+                    onChangeText = { (text) => this.setState({ email:text }) }
+                    value = { this.state.email }
+                />
+                <TouchableHighlight underlayColor='#31e981' onPress={this.submitDetails} >
+                        <Text style={styles.buttons}>
+                                Submit
+                        </Text>
+                </TouchableHighlight>
+                <TouchableHighlight underlayColor='#31e981' onPress={this.clearFields} >
+                        <Text style={styles.buttons}>
+                            Clear
+                        </Text>
+                </TouchableHighlight>
             </View>
         )
     }
@@ -20,5 +64,25 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
+      alignItems: 'center',
+      paddingBottom: '35%'
+    },
+    heading: {
+        flex: 1,
+        fontSize: 18
+    },
+    inputs: {
+        flex: 1,
+        padding: 10,
+        width: '80%'
+    },
+    multiInput: {
+        flex: 2,
+        paddingTop: 10,
+        width: '75%'
+    },
+    buttons: {
+        marginTop: 15,
+        fontSize:16
     },
 });
--- app/views/Home.js
+++ app/views/Home.js
@@ -3,14 +3,16 @@ import { StyleSheet, View, Text } from 'react-native';
 import { Header } from '../sections/Header';
 import { Menu } from '../sections/Menu';
 import { Slider } from '../sections/Slider';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export class Home extends React.Component {
     render() {
         return(
-            <View style={styles.container}>
-                <Header style={styles.section1} message = 'Press to Login' />
+            <View style = { styles.container }>
+                <Header style = { styles.section1 } message = 'Press to Login' />
                 <Slider/>
-                <Menu/>
+                <Menu navigation = { this.props.navigation }/>
             </View>
         )
     }
